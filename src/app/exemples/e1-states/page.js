06d7f938255835ea47e1eb2496f12f0629567515
Antoine F Moevus <2613844+ant1moevus@users.noreply.github.com>
2024-11-13 20:25:57 -0500

    Create structure for the exemples in the class

---
+++ src/app/exemples/e1-states/page.js
@@ -0,0 +1,9 @@
+export default function DemoStates() {
+
+    return (
+        <>
+        <h1>Démo sur les états</h1>
+        <p>allo</p>
+        </>
+    )
+}
---
+++ src/app/exemples/page.js
@@ -0,0 +1,11 @@
+export default function Contacts() {
+
+    return (
+        <>
+            <h1 className="text-4xl m-4"> Exemples </h1>
+            <ul className="flex justify-start text-xl text-emerald-300 w-1/2">
+                <li><a href="/exemples/e1-states">Exemple states</a></li>
+            </ul>
+        </>
+    )
+}
