90e4f3462ed0e40c9df3c4ee8f20183ab6c9df78
Antoine F Moevus <2613844+ant1moevus@users.noreply.github.com>
2024-11-16 12:44:53 -0500

    Install uuidv4 and play with conditionnal rendering

--- src/app/exemples/e2-liste-items/page.js
+++ src/app/exemples/e2-liste-items/page.js
@@ -1,17 +1,19 @@
 'use client'
 
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+
 import VignetteProduit from "@/app/components/VignetteProduit"
 
 
 export default function ListeProduits() {
     const [products, setProducts] = useState([
-        { id:1, category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-        { id:2,category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-        { id:3,category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-        { id:4,category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-        { id:5,category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-        { id:6,category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+        { id: 1, category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+        { id: 2, category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+        { id: 3, category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+        { id: 4, category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+        { id: 5, category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+        { id: 6, category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
     ]);
 
     const deleteItem = () => {
@@ -30,10 +32,10 @@ export default function ListeProduits() {
         setProducts((prevProducts) => {
             const rd = Math.floor(Math.random() * products.length)
             const newProducts = [...prevProducts]
-            let ids = prevProducts.map((item)=>{return item.id})
-            const maxID = Math.max(...ids)
-            console.log(maxID)
-            newProducts.splice(rd, 0, {id: maxID+1 , category: "NA", price: "-1$", stocked: false, name: "NA" })
+            // let ids = prevProducts.map((item)=>{return item.id})
+            // const maxID = Math.max(...ids) //alternative hands-on de uuidv4
+            // // console.log(maxID) 
+            newProducts.splice(rd, 0, { id: uuidv4(), category: "NA", price: "-1$", stocked: false, name: "NA" })
             return newProducts
         })
     }
@@ -42,18 +44,22 @@ export default function ListeProduits() {
     return (
         <>
             <button className='bg-slate-200 text-xl w-1/3 m-1' onClick={deleteItem}>Supprimmer</button>
-             <button className='bg-slate-200 text-xl w-1/3 m-1' onClick={addItem}>Ajouter</button>
+            <button className='bg-slate-200 text-xl w-1/3 m-1' onClick={addItem}>Ajouter</button>
             <div className='flex flex-row flex-wrap h-1/2'>
                 {products && products.map((item, i) => {
-                    return (
-                     <VignetteProduit item={item} key={item.id} listID={i} />
-                    )
+                    if (products.length < 8) {
+                        return (item.stocked && (
+                            <VignetteProduit item={item} key={item.id} listID={item.id} />
+                        ))
+                    } else {
+                        return (!item.stocked && (
+                            <VignetteProduit item={item} key={item.id} listID={item.id} />
+                        ))
+                    }
                 })
 
                 }
             </div>
-
-
         </>
     )
 }
