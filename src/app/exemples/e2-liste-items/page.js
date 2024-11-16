a8cb172c0357df8f1a6827a4a3fa4f898e3a3081
Antoine F Moevus <2613844+ant1moevus@users.noreply.github.com>
2024-11-16 12:30:51 -0500

    Add autoincrement key for table

--- src/app/exemples/e2-liste-items/page.js
+++ src/app/exemples/e2-liste-items/page.js
@@ -6,16 +6,15 @@ import VignetteProduit from "@/app/components/VignetteProduit"
 
 export default function ListeProduits() {
     const [products, setProducts] = useState([
-        { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-        { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-        { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-        { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-        { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-        { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+        { id:1, category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+        { id:2,category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+        { id:3,category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+        { id:4,category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+        { id:5,category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+        { id:6,category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
     ]);
 
     const deleteItem = () => {
-
         setProducts((prevProducts) => {
             const rd = Math.floor(Math.random() * products.length)
             const newProducts = [...products]
@@ -31,7 +30,10 @@ export default function ListeProduits() {
         setProducts((prevProducts) => {
             const rd = Math.floor(Math.random() * products.length)
             const newProducts = [...prevProducts]
-            newProducts.splice(rd, 0, { category: "NA", price: "-1$", stocked: false, name: "NA" })
+            let ids = prevProducts.map((item)=>{return item.id})
+            const maxID = Math.max(...ids)
+            console.log(maxID)
+            newProducts.splice(rd, 0, {id: maxID+1 , category: "NA", price: "-1$", stocked: false, name: "NA" })
             return newProducts
         })
     }
@@ -44,7 +46,7 @@ export default function ListeProduits() {
             <div className='flex flex-row flex-wrap h-1/2'>
                 {products && products.map((item, i) => {
                     return (
-                     <VignetteProduit item={item} key={i} listID={i} />
+                     <VignetteProduit item={item} key={item.id} listID={i} />
                     )
                 })
 
