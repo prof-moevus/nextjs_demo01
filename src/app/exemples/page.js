a3d05f6d33abe431636d4fbd9745388235ba26e6
Antoine F Moevus <2613844+ant1moevus@users.noreply.github.com>
2024-11-27 19:13:52 -0500

    🐛🔥 Fix excaped caracter with &apos

--- src/app/exemples/page.js
+++ src/app/exemples/page.js
@@ -9,7 +9,7 @@ export default function Contacts() {
             <h1 className="text-4xl m-4"> Exemples </h1>
             <ul className="text-xl text-emerald-300 w-1/2">
                 <li><a href="/exemples/e1-states">Exemple states</a></li>
-                <li><a href="/exemples/e2-liste-items">Exemple liste d'objets</a></li>
+                <li><a href="/exemples/e2-liste-items">Exemple liste d &apos objets</a></li>
                 <li><a href="/exemples/e3-useEffect">Exemple du hook useEffect()</a></li>
             </ul>
         </>
