80e65bd5c73c51c6c8587c88f3c599fdd9ba1c00
Antoine F Moevus <2613844+ant1moevus@users.noreply.github.com>
2024-11-27 19:17:28 -0500

    Change bg color for dark indigo

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-indigo-300`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-indigo-800`}
       >
         {children}
       </body>
